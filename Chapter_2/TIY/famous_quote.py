625b9e529cb730671de0349499bdacd384a636dd
rosilioroman <rosilioroman@gmail.com>
2023-05-17 19:20:57 -0600

    Made some progress on Chapter_2

---
+++ Chapter_2/TIY/famous_quote.py
@@ -0,0 +1,8 @@
+# Variable for the author's name
+author = "Farengar Secret-Fire"
+
+# Variable for the quote
+quote = "A sure mark of a fool is to dismiss anything outside of his experience as impossible."
+
+# Formatted print statement (notice the use of single and double quotes)
+print(f'This is one of my favorite quotes!\n{author} once said, "{quote}"')
---
+++ Chapter_2/TIY/name_cases.py
@@ -0,0 +1,5 @@
+# Ask the user for his/her name
+name = input("Hi there! What is your full name? ")
+
+# Print the name in UPPER, lower, and Title Case
+print(f"UPPER: {name.upper()}\nlower: {name.lower()}\nTitle Case: {name.title()}")
